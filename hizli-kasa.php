e3d518d7c5162fd557ac300237a916b14a5093f7
Seyfullah Kurt <seyfullahkurt9@gmail.com>
2026-04-18 19:17:07 +0300

    düzeltme yaması11

--- hizli-kasa.php
+++ hizli-kasa.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Hızlı Kasa
  * Description: WooCommerce için sadece barkod ve enter tuşuyla çalışan hızlı POS sistemi.
- * Version: 3.2.1
+ * Version: 3.2.2
  * Author: Seyfullah Kurt
  */
 
@@ -10,7 +10,7 @@ if (!defined('ABSPATH'))
 	exit;
 
 // Sabitler
-define('HIZLI_KASA_VERSION', '3.2.1');
+define('HIZLI_KASA_VERSION', '3.2.2');
 define('HIZLI_KASA_PATH', plugin_dir_path(__FILE__));
 define('HIZLI_KASA_URL', plugin_dir_url(__FILE__));
 
