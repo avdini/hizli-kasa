3eb72892b4c5e0176ee0f72159459795d2311067
Seyfullah Kurt <seyfullahkurt9@gmail.com>
2026-04-24 03:01:50 +0300

    kasa görünüm düzeltme12

--- hizli-kasa.php
+++ hizli-kasa.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Hızlı Kasa
  * Description: avdini için hızlı POS sistemi.
- * Version: 4.2.41.12.20
+ * Version: 4.2.41.12.21
  * Author: Seyfullah Kurt
  */
 
@@ -10,7 +10,7 @@ if (!defined('ABSPATH'))
     exit;
 
 // Sabitler
-define('HIZLI_KASA_VERSION', '4.2.41.12.20');
+define('HIZLI_KASA_VERSION', '4.2.41.12.21');
 define('HIZLI_KASA_PATH', plugin_dir_path(__FILE__));
 define('HIZLI_KASA_URL', plugin_dir_url(__FILE__));
 
