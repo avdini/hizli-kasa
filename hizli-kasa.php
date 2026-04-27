b63b3edde3429466853433210615cbfe5ba1c3f4
Seyfullah Kurt <seyfullahkurt9@gmail.com>
2026-04-27 18:20:38 +0300

    barkod düzeltme3

--- hizli-kasa.php
+++ hizli-kasa.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Hızlı Kasa
  * Description: avdini için hızlı POS sistemi.
- * Version: 4.4.15
+ * Version: 4.4.16
  * Author: Seyfullah Kurt
  */
 
@@ -10,7 +10,7 @@ if (!defined('ABSPATH'))
     exit;
 
 // Sabitler
-define('HIZLI_KASA_VERSION', '4.4.15');
+define('HIZLI_KASA_VERSION', '4.4.16');
 define('HIZLI_KASA_PATH', plugin_dir_path(__FILE__));
 define('HIZLI_KASA_URL', plugin_dir_url(__FILE__));
 
