c58287b8da71c69bc952cece5f37d89d1bffff31
Seyfullah Kurt <seyfullahkurt9@gmail.com>
2026-06-10 14:34:16 +0300

    test2

--- assets/js/modules/stock-terminal.js
+++ assets/js/modules/stock-terminal.js
@@ -327,6 +327,7 @@
                             self.openEditModal(product);
                         }
                         */
+                    }
                 });
 
                 // Depo Kodu Değişikliği Event Delegation
