64634f01720cd57ebabe97456121bd7dcd617ed4
Seyfullah Kurt <seyfullahkurt9@gmail.com>
2026-08-04 19:48:47 +0300

    feat(barcode): add attribute support for simple product barcode labels and bump version to 12.50.0

--- includes/classes/class-barcode-helper.php
+++ includes/classes/class-barcode-helper.php
@@ -57,13 +57,6 @@ class Hizli_Kasa_Barcode_Helper {
      * Kategori "Ayakkabı" ise "Numara" etiketi kullanılır.
      */
     private static function get_formatted_attributes($product) {
-        $attributes = [];
-        
-        if (!$product->is_type('variation')) {
-            return $attributes;
-        }
-
-        $variation_attributes = $product->get_variation_attributes();
         $formatted = [
             'color'       => '',
             'color_label' => 'Renk',
@@ -71,68 +64,107 @@ class Hizli_Kasa_Barcode_Helper {
             'label'       => 'Beden'
         ];
 
-        $parent_id = $product->get_parent_id();
-        $parent    = wc_get_product($parent_id);
-
-        // Kategori kontrolü (Ayakkabı mı?)
-        $terms = get_the_terms($parent_id, 'product_cat');
-        if ($terms && !is_wp_error($terms)) {
-            foreach ($terms as $term) {
-                if (mb_stripos($term->name, 'Ayakkabı') !== false) {
-                    $formatted['label'] = 'Numara';
-                    break;
-                }
-            }
+        if (!$product) {
+            return $formatted;
         }
 
         $unmapped = [];
 
-        foreach ($variation_attributes as $attr_name => $attr_value) {
-            if (empty($attr_value)) continue;
+        if ($product->is_type('variation')) {
+            $variation_attributes = $product->get_variation_attributes();
+            $parent_id = $product->get_parent_id();
+            $parent    = wc_get_product($parent_id);
 
-            $taxonomy   = str_replace('attribute_', '', $attr_name);
-            $clean_name = str_replace('pa_', '', $taxonomy);
-
-            // Değeri al (term name)
-            $term          = get_term_by('slug', $attr_value, $taxonomy);
-            $display_value = $term ? $term->name : $attr_value;
-
-            // İnsan tarafından okunabilir başlık (ör: "Kapasite", "Hafıza")
-            $display_label = $parent ? wc_attribute_label($taxonomy, $parent) : ucfirst(str_replace('-', ' ', $clean_name));
-
-            if (mb_stripos($clean_name, 'renk') !== false || mb_stripos($clean_name, 'color') !== false) {
-                $formatted['color']       = mb_convert_case($display_value, MB_CASE_TITLE, "UTF-8");
-                $formatted['color_label'] = $display_label;
-            } elseif (mb_stripos($clean_name, 'beden') !== false || mb_stripos($clean_name, 'size') !== false || mb_stripos($clean_name, 'numara') !== false) {
-                $formatted['size']  = mb_strtoupper($display_value, "UTF-8");
-                $formatted['label'] = $display_label;
-            } elseif (mb_stripos($clean_name, 'ölçü') !== false || mb_stripos($clean_name, 'olcu') !== false || mb_stripos($clean_name, 'boy') !== false) {
-                $formatted['size']  = mb_strtoupper($display_value, "UTF-8");
-                $formatted['label'] = $display_label;
-            } else {
-                $unmapped[] = [
-                    'label' => $display_label,
-                    'value' => $display_value
-                ];
-            }
-        }
-
-        // Eğer renk varyasyonda tanımlı değilse parent (ana) üründen çekmeyi dene (Used for variations kapalıysa)
-        if (empty($formatted['color']) && $parent) {
-            foreach ($parent->get_attributes() as $attr_name => $attribute) {
-                $label = wc_attribute_label($attr_name, $parent);
-                if (mb_stripos($label, 'renk') !== false || mb_stripos($label, 'color') !== false) {
-                    $display_value = $parent->get_attribute($attr_name);
-                    if (!empty($display_value)) {
-                        $formatted['color']       = mb_convert_case($display_value, MB_CASE_TITLE, "UTF-8");
-                        $formatted['color_label'] = $label;
+            $terms = get_the_terms($parent_id, 'product_cat');
+            if ($terms && !is_wp_error($terms)) {
+                foreach ($terms as $term) {
+                    if (mb_stripos($term->name, 'Ayakkabı') !== false) {
+                        $formatted['label'] = 'Numara';
                         break;
+                    }
+                }
+            }
+
+            foreach ($variation_attributes as $attr_name => $attr_value) {
+                if (empty($attr_value)) continue;
+
+                $taxonomy   = str_replace('attribute_', '', $attr_name);
+                $clean_name = str_replace('pa_', '', $taxonomy);
+
+                $term          = get_term_by('slug', $attr_value, $taxonomy);
+                $display_value = $term ? $term->name : $attr_value;
+
+                $display_label = $parent ? wc_attribute_label($taxonomy, $parent) : ucfirst(str_replace('-', ' ', $clean_name));
+
+                if (mb_stripos($clean_name, 'renk') !== false || mb_stripos($clean_name, 'color') !== false) {
+                    $formatted['color']       = mb_convert_case($display_value, MB_CASE_TITLE, "UTF-8");
+                    $formatted['color_label'] = $display_label;
+                } elseif (mb_stripos($clean_name, 'beden') !== false || mb_stripos($clean_name, 'size') !== false || mb_stripos($clean_name, 'numara') !== false) {
+                    $formatted['size']  = mb_strtoupper($display_value, "UTF-8");
+                    $formatted['label'] = $display_label;
+                } elseif (mb_stripos($clean_name, 'ölçü') !== false || mb_stripos($clean_name, 'olcu') !== false || mb_stripos($clean_name, 'boy') !== false) {
+                    $formatted['size']  = mb_strtoupper($display_value, "UTF-8");
+                    $formatted['label'] = $display_label;
+                } else {
+                    $unmapped[] = [
+                        'label' => $display_label,
+                        'value' => $display_value
+                    ];
+                }
+            }
+
+            if (empty($formatted['color']) && $parent) {
+                foreach ($parent->get_attributes() as $attr_name => $attribute) {
+                    $label = wc_attribute_label($attr_name, $parent);
+                    if (mb_stripos($label, 'renk') !== false || mb_stripos($label, 'color') !== false) {
+                        $display_value = $parent->get_attribute($attr_name);
+                        if (!empty($display_value)) {
+                            $formatted['color']       = mb_convert_case($display_value, MB_CASE_TITLE, "UTF-8");
+                            $formatted['color_label'] = $label;
+                            break;
+                        }
+                    }
+                }
+            }
+        } else {
+            $terms = get_the_terms($product->get_id(), 'product_cat');
+            if ($terms && !is_wp_error($terms)) {
+                foreach ($terms as $term) {
+                    if (mb_stripos($term->name, 'Ayakkabı') !== false) {
+                        $formatted['label'] = 'Numara';
+                        break;
+                    }
+                }
+            }
+
+            $raw_attributes = $product->get_attributes();
+            if (!empty($raw_attributes)) {
+                foreach ($raw_attributes as $attr_name => $attribute) {
+                    $display_value = $product->get_attribute($attr_name);
+                    if (empty($display_value)) continue;
+
+                    $display_label = wc_attribute_label($attr_name, $product);
+                    $clean_name    = str_replace('pa_', '', $attr_name);
+
+                    if (mb_stripos($clean_name, 'renk') !== false || mb_stripos($clean_name, 'color') !== false || mb_stripos($display_label, 'renk') !== false || mb_stripos($display_label, 'color') !== false) {
+                        $formatted['color']       = mb_convert_case($display_value, MB_CASE_TITLE, "UTF-8");
+                        $formatted['color_label'] = $display_label;
+                    } elseif (mb_stripos($clean_name, 'beden') !== false || mb_stripos($clean_name, 'size') !== false || mb_stripos($clean_name, 'numara') !== false || mb_stripos($display_label, 'beden') !== false || mb_stripos($display_label, 'size') !== false || mb_stripos($display_label, 'numara') !== false) {
+                        $formatted['size']  = mb_strtoupper($display_value, "UTF-8");
+                        $formatted['label'] = $display_label;
+                    } elseif (mb_stripos($clean_name, 'ölçü') !== false || mb_stripos($clean_name, 'olcu') !== false || mb_stripos($clean_name, 'boy') !== false || mb_stripos($display_label, 'ölçü') !== false || mb_stripos($display_label, 'boy') !== false) {
+                        $formatted['size']  = mb_strtoupper($display_value, "UTF-8");
+                        $formatted['label'] = $display_label;
+                    } else {
+                        $unmapped[] = [
+                            'label' => $display_label,
+                            'value' => $display_value
+                        ];
                     }
                 }
             }
         }
 
-        // Maplenmemiş özel varyasyon öznitelikleri için dinamik yuva ataması
         if (!empty($unmapped)) {
             if (empty($formatted['color']) && isset($unmapped[0])) {
                 $first = array_shift($unmapped);
