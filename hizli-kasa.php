85d79b31fe48f0662ad189f42d38fb331ff179bf
Seyfullah Kurt <seyfullahkurt9@gmail.com>
2026-08-05 17:37:14 +0300

    style(reports): change Depo meta tag color from blue to neutral grey v12.57.1

--- hizli-kasa.php
+++ hizli-kasa.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Hızlı Kasa
  * Description: avdini için hızlı POS sistemi.
- * Version: 12.57.0
+ * Version: 12.57.1
  * Author: Seyfullah Kurt
  * Requires Plugins: woocommerce
  * Requires at least: 5.8
