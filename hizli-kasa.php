621735bde8aa5dcebb1fe9d4d6da76e232ba86a0
Seyfullah Kurt <seyfullahkurt9@gmail.com>
2026-08-05 17:10:34 +0300

    feat(reports): transparent card container, 100% full width table card, and floating bubble header UI v12.57.0

--- hizli-kasa.php
+++ hizli-kasa.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Hızlı Kasa
  * Description: avdini için hızlı POS sistemi.
- * Version: 12.56.4
+ * Version: 12.57.0
  * Author: Seyfullah Kurt
  * Requires Plugins: woocommerce
  * Requires at least: 5.8
