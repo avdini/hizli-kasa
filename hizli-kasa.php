f2c58e7a94e0bd9dfb276b63373a87370b1d92c3
Seyfullah Kurt <seyfullahkurt9@gmail.com>
2026-08-05 16:55:50 +0300

    feat(reports): premium redesign for all report containers, tables, buttons, headers and pagination v12.56.0

--- hizli-kasa.php
+++ hizli-kasa.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Hızlı Kasa
  * Description: avdini için hızlı POS sistemi.
- * Version: 12.55.4
+ * Version: 12.56.0
  * Author: Seyfullah Kurt
  * Requires Plugins: woocommerce
  * Requires at least: 5.8
